6a9d16b4e1ec4e317a11abd635180dd7f8aed1a8
carlocarlen <carlocarlen@gmail.com>
2024-08-26 17:19:50 +0300

    next char

--- src/app/generator/generator.service.ts
+++ src/app/generator/generator.service.ts
@@ -6,6 +6,9 @@ import { Injectable } from '@angular/core';
 export class GeneratorService {
 
   private seed: string[] = ['1','2','3','4','5','1','2','3','4','5'];
+  private currentBatch: string[] = this.seed.slice();
+  private currentIndex = 0;
+  private maxIndex = 10;
 
   constructor() { }
 
@@ -15,5 +18,21 @@ export class GeneratorService {
     }
 
     this.seed = (input5 + input5).split('');
+    this.currentBatch = this.seed.slice();
+  }
+
+  // This is the magic algorithm 
+  nextBatch(inputBatch: string[]): string[] {
+    const everySecond = inputBatch.filter((_, index) => index %2 === 0);
+    return everySecond.concat(everySecond);
+  }
+
+  nextChar(): string {
+    this.currentIndex++;
+    if (this.currentIndex === this.maxIndex) {
+      this.currentBatch = this.nextBatch(this.currentBatch);
+      this.currentIndex = 0;
+    }
+    return this.currentBatch[this.currentIndex];
   }
 }
