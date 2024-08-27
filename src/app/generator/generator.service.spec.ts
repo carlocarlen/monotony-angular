684ada4968f91f767f407cc54c2d44f1282ebd9a
carlocarlen <carlocarlen@gmail.com>
2024-08-27 16:49:01 +0300

    that would be a test

--- src/app/generator/generator.service.spec.ts
+++ src/app/generator/generator.service.spec.ts
@@ -13,4 +13,10 @@ describe('GeneratorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('shold produce the correct sequence', () => {
+    expect(service.nextChar()).toBe('1');
+    expect(service.nextChar()).toBe('2');
+    // ... and so on 
+  })
 });
